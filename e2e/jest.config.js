24be4024f6a9d7cc9f760478bc9a64f914a78986
RECTOR <rector@rectorspace.com>
2026-09-25 10:39:47 +0700

    fix(e2e): drop dangling setupFilesAfterEnv reference (setup.ts removed in prior commit)

--- e2e/jest.config.js
+++ e2e/jest.config.js
@@ -6,7 +6,6 @@ module.exports = {
   maxWorkers: 1,
   globalSetup: 'detox/runners/jest/globalSetup',
   globalTeardown: 'detox/runners/jest/globalTeardown',
-  setupFilesAfterEnv: ['<rootDir>/e2e/setup.ts'],
   reporters: ['detox/runners/jest/reporter'],
   testEnvironment: 'detox/runners/jest/testEnvironment',
   verbose: true,
